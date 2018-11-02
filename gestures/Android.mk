b789ad8aaa3e579d77b1ba13dfc8c780cc9cbe12
daanieldasilvaa <aka.mdsg@gmail.com>
2018-11-02 06:12:53 +0000

    ocn: gestures: clean up, set LOCAL_PRIVATE_PLATFORM_APIS and LOCAL_USE_AAPT2

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -1,15 +1,24 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := GestureHandler
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
     android-support-v7-appcompat \
-    android-support-v14-preference \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
@@ -21,12 +30,6 @@ LOCAL_RESOURCE_DIR := \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_PACKAGE_NAME := GestureHandler
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
